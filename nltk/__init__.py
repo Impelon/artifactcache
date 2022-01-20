13c6e967dc4bebaa06f3e2ad16fa6a4d5f33753d
Impelon <ovi.tatar@googlemail.com>
2022-01-20 23:59:40 +0100

    nltk, huggingface: restructure library-caches
    This also moves the transformers cache into the new overarching
    huggingface cache.

--- nltk/__init__.py
+++ nltk/__init__.py
@@ -2,9 +2,6 @@ from ..__init__ import CacheWithEnv
 
 import os.path
 
-CACHE = CacheWithEnv(os.path.dirname(os.path.realpath(__file__)), "NLTK_DATA")
+__all__ = ["cache"]
 
-path = CACHE.path
-is_enabled = CACHE.is_enabled
-enable = CACHE.enable
-disable = CACHE.disable
+cache = CacheWithEnv(os.path.dirname(os.path.realpath(__file__)), "NLTK_DATA")
