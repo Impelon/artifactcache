e5fe02b59b669e3dd7c94813492246563877f329
Impelon <ovi.tatar@googlemail.com>
2023-10-06 00:50:09 +0200

    core: make importable from top-level directory

---
+++ __init__.py
@@ -0,0 +1,9 @@
+"""Just a slightly hacky solution to make importing the top-level directory
+behave the same as importing the package.
+
+This is mainly intended to allow using this library from within a git
+submodule instead of a pip installation.
+"""
+
+from .artifactcache import __path__
+from .artifactcache import *
