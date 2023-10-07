3e853c66cdc54c362ff8f4988b468961989039b4
Impelon <ovi.tatar@googlemail.com>
2023-10-07 19:04:15 +0200

    core: release as installable package

---
+++ setup.py
@@ -0,0 +1,13 @@
+from setuptools import find_packages, setup
+
+setup(
+    name="artifactcache",
+    version="0.1",
+    description="A minimalistic python package providing a convenient API to change the default download location for different libraries.",
+    url="https://github.com/Impelon/artifactcache",
+    author="Impelon",
+    author_email="development@ovi-tatar.de",
+    license="BSD-2-Clause",
+    packages=find_packages(),
+    zip_safe=False,
+)
