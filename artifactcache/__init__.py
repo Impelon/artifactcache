7a1dd5a83670273b941057de861c839d21dd79de
Impelon <ovi.tatar@googlemail.com>
2023-10-07 18:57:19 +0200

    core: add version detection code
    If this library is installed as a module, this code will detect the
    version and provide it as a variable.

--- artifactcache/__init__.py
+++ artifactcache/__init__.py
@@ -3,3 +3,9 @@ libraries."""
 
 from ._cachetypes import *
 from ._centralized_location import *
+from ._version import package_version
+
+__version__ = package_version
+if package_version is None:
+    __version__ = "unknown"
+del package_version
---
+++ artifactcache/_version.py
@@ -0,0 +1,27 @@
+package_name = "artifactcache"
+package_version = None
+
+if package_version is None:
+    importlib_metadata = None
+    try:
+        import importlib.metadata as importlib_metadata
+    except ImportError:
+        try:
+            import importlib_metadata
+        except ImportError:
+            pass
+    if importlib_metadata:
+        try:
+            package_version = importlib_metadata.version(package_name)
+        except importlib_metadata.PackageNotFoundError:
+            pass
+
+if package_version is None:
+    try:
+        import pkg_resources
+        try:
+            package_version = pkg_resources.require(package_name)[0].version
+        except pkg_resources.DistributionNotFound:
+            pass
+    except ImportError:
+        pass
